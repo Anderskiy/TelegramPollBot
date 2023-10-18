3540e765d968fb459f9a4132b3dd46217298b6b4
Roman <ander.men70@gmail.com>
2023-10-18 20:33:07 +0300

    Добавление клавиатуры

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import asyncio
 
-from aiogram import Bot, Dispatcher, F
-from aiogram.filters import Command, CommandStart
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
 from aiogram.types import Message
+
+from keyboard import main_kb
 
 bot = Bot("6158954004:AAHK5Fe_3iiDDMZD_-Bv5sHNjiA00-1Zzjs")
 dp = Dispatcher()
@@ -10,11 +12,15 @@ dp = Dispatcher()
 @dp.message(CommandStart())
 async def start(message: Message):
     print(message)
-    await message.answer(f"💖 Спасибо что воспользовались нашим ботом, {message.from_user.first_name}")
+    await message.answer(f"💖 Спасибо что воспользовались нашим ботом, {message.from_user.first_name}",
+                         reply_markup=main_kb)
 
-@dp.message(Command("about"))
-async def about(message: Message):
-    await message.answer("Что умеет этот бот?")
+@dp.message()
+async def echo(message: Message):
+    msg = message.text.lower()
+
+    if msg == "что я умею?":
+        await message.answer("Пока что ничего, но очень скоро буду рассказывать про космос!")
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
